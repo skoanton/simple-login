1243c370283603db74fb43100dfee510a8c4aaaa
skoanton <antonhakanssonn@gmail.com>
2024-04-18 13:44:01 +0200

    installed and imported robotic

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 const router = createBrowserRouter([
   {
     path: "/",
